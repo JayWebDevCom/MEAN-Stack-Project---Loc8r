2fb57c1110d91bb3f3b4b6d734a84cf3e744381b
JayWebDevCom <jaiye.s@gmail.com>
2017-06-25 00:28:44 +0100

    refactor unused view static function

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ var routesApi = require('./app_api/routes/index');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
 app.set('views', path.join(__dirname, 'app_server', 'views'))
 app.set('view engine', 'jade');
 
