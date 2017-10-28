0f334d430146752ca08a1ed29b0db044866e1573
JayWebDevCom <jaiye.s@gmail.com>
2017-10-28 01:30:59 +0100

    Add geolocation service and functionality

--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -1,30 +1,48 @@
 angular.module("loc8rApp", []);
 
-var locationListCtrl = function($scope, $http) {
-  $scope.message = "Searching for nearby places";
-  $http({
-    method: "GET",
-    url: "/api/locations?lng=-0.79&lat=51.3&maxDistance=20"
-  }).then(function(data) {
-    $scope.message = data.data.length > 0 ? "" : "No locations found";
-    $scope.data = { locations: data.data };
-  }),
-    function(error) {
-      $scope.message = "Sorry something has gone wrong";
-    };
-};
-
-// var locationListCtrl = function($scope, loc8rData) {
+// var locationListCtrl = function($scope, $http, geolocation) {
 //   $scope.message = "Searching for nearby places";
-//   loc8rData
-//     .success(function(data) {
-//       $scope.message = data.length > 0 ? "" : "No locations found";
-//       $scope.data = { locations: data };
-//     })
-//     .error(function(e) {
+//   $http({
+//     method: "GET",
+//     url: "/api/locations?lng=-0.79&lat=51.3&maxDistance=20"
+//   }).then(function(data) {
+//     $scope.message = data.data.length > 0 ? "" : "No locations found";
+//     $scope.data = { locations: data.data };
+//   }),
+//     function(error) {
 //       $scope.message = "Sorry something has gone wrong";
-//     });
+//     };
 // };
+
+var locationListCtrl = function($scope, loc8rData, geolocation) {
+  $scope.message = "Checking your location";
+
+  $scope.getData = function(position) {
+    $scope.message = "Searching for nearby places...";
+    loc8rData
+      .success(function(data) {
+        $scope.message = data.length > 0 ? "" : "No locations found";
+        $scope.data = { locations: data };
+      })
+      .error(function(e) {
+        $scope.message = "Sorry something has gone wrong";
+      });
+  };
+
+  $scope.showError = function(error) {
+    $scope.$apply(function() {
+      $scope.message = error.message;
+    });
+  };
+
+  $scope.noGeo = function() {
+    $scope.$apply(function() {
+      $scope.message = "Geolocation not supported by this browser";
+    });
+  };
+
+  geolocation.getPosition($scope.getData, $scope.showError, $scope.noGeo);
+};
 
 var _isNumeric = function(n) {
   return !isNaN(parseFloat(n) && isFinite(n));
@@ -59,11 +77,26 @@ var ratingStars = function() {
 
 var loc8rData = function($http) {
   return $http.get("/api/locations?lng=-0.79&lat=51.3&maxDistance=20");
-}
+};
+
+var geolocation = function() {
+  var getPosition = function(cbSuccess, cbError, cbNoGeo) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(cbSuccess, cbError);
+    } else {
+      cbNoGeo();
+    }
+  };
+  return {
+    getPosition: getPosition
+  };
+};
+
 
 angular
   .module("loc8rApp")
   .controller("locationListCtrl", locationListCtrl)
   .filter("formatDistance", formatDistance)
   .directive("ratingStars", ratingStars)
-  .service("loc8rData", loc8rData);
+  .service("loc8rData", loc8rData)
+  .service("geolocation", geolocation);
