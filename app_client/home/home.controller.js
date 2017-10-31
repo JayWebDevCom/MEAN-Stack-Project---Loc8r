ec68fac9d92be0fbb883d3e20ca1db133f338f98
JayWebDevCom <jaiye.s@gmail.com>
2017-10-31 20:35:56 +0000

    replace $scope use with viewModel variable - controllerAs syntax

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -1,14 +1,15 @@
-angular.module('loc8rApp')
-.controller('homeCtrl', homeCtrl);
+angular.module("loc8rApp").controller("homeCtrl", homeCtrl);
 
-function homeCtrl($scope) {
-  $scope.pageHeader = {
-    title: 'Loc8r',
-    strapline: 'Find a couple of places with wifi for you to work :)'
+function homeCtrl() {
+  var viewModel = this;
+  viewModel.pageHeader = {
+    title: "Loc8r",
+    strapline: "Find a couple of places with wifi for you to work :)"
   };
-  $scope.sidebar = {
-    content: 'Looking for wifi and a seat? Loc8r helps you find places to \
+  viewModel.sidebar = {
+    content:
+      "Looking for wifi and a seat? Loc8r helps you find places to \
      work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r \
-     help you find the place you\'re looking for.'
+     help you find the place you're looking for."
   };
 }
